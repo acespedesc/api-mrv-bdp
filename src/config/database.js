f53eb4c70367b7ad71d26aba2e36dcce55a6ddf6
acespedesc <arcespedesc@gmail.com>
2024-10-03 20:57:02 -0400

    modificando config

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,6 @@ export  const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        dialect: 'postgres',
     }
 );
